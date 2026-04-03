e635ffe3fc32cdf718e9b5b1a50663df3645da26
NguyenTruongDucHuy <ngduchuy1612@gmail.com>
2026-04-03 20:17:34 +0700

    update

--- server/utils/MongooseUtil.js
+++ server/utils/MongooseUtil.js
@@ -2,13 +2,14 @@
 const mongoose = require('mongoose');
 const MyConstants = require('./MyConstants');
 
-const uri = `mongodb+srv://${MyConstants.DB_USER}:${MyConstants.DB_PASS}@${MyConstants.DB_SERVER}/${MyConstants.DB_DATABASE}`;
+// Ưu tiên lấy URI từ biến môi trường của Render, nếu không có thì dùng MyConstants
+const uri = process.env.MONGODB_URI || `mongodb+srv://${MyConstants.DB_USER}:${MyConstants.DB_PASS}@${MyConstants.DB_SERVER}/${MyConstants.DB_DATABASE}`;
 
 async function connectMongo() {
   try {
     await mongoose.connect(uri);
     console.log(
-      `Connected to ${MyConstants.DB_SERVER}/${MyConstants.DB_DATABASE}`
+      `Connected to MongoDB successfully!`
     );
   } catch (error) {
     console.error('MongoDB connection error:', error);
